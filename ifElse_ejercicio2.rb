404d7ebe8339e79d7bca48613db403351d856481
Matias Jara <mjara.figueroa@gmail.com>
2017-08-01 14:37:15 -0400

    ejercicio 2 de if and else

---
+++ ifElse_ejercicio2.rb
@@ -0,0 +1,12 @@
+=begin Hacer que se cumpla la condición modificando la línea 3.
+
+a = 2
+if a == 5
+  puts 'La condición es verdadera.'
+end
+=end
+
+a = 5
+if a == 5
+  puts 'La condición es verdadera.'
+end
