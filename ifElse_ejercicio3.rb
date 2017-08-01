9236db1d2fbfc388fd149c3b8e05d3072473e7ec
Matias Jara <mjara.figueroa@gmail.com>
2017-08-01 14:39:32 -0400

    ejercicio 3 de if and else

---
+++ ifElse_ejercicio3.rb
@@ -0,0 +1,9 @@
+=begin Hacer un refactoring, transformando las líneas de la 5 a la 7 en una sola.
+
+a = 'X9-by'
+if a == 'X9-by'
+  puts 'HOLA!'
+end
+=end
+a = 'X9-by'
+puts 'HOLA!' if a == 'X9-by'
