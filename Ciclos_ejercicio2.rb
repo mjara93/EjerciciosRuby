b704e7b135a81e59b753ba9e73740f2902325606
Matias Jara <mjara.figueroa@gmail.com>
2017-08-01 15:02:25 -0400

    ejercicio 2 de ciclos

---
+++ Ciclos_ejercicio2.rb
@@ -0,0 +1,9 @@
+=begin En el siguiente código reemplaza la instrucción 'while' por 'times'.
+i = 0
+while i < 10
+  puts "Iteración #{i}"
+  i = i + 1
+end
+=end
+
+10.times {|x| puts "Iteración #{x}"}
